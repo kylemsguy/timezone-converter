85b9c4afee244461659ff327a0efc35b9f7d6180
Kyle Zhou <kylezhou2002@hotmail.com>
2015-07-27 00:37:54 -0700

    added nophp and changed regular to use dropdown menu for time zone

--- index.php
+++ index.php
@@ -1,18 +1,43 @@
 <?php
 date_default_timezone_set('Etc/UTC');
-$time = "Error!";
-if(isset($_GET['time']) && isset($_GET['offset'])) {
-  $time = strtotime($_GET['time']) - intval($_GET['offset']) * 3600;
+
+function offsetToHrsMins($offset) {
+	$sign = "+";
+	$hours = floor($offset / 3600);
+	$mins = ($offset / 3600 - $hours) * 60;
+	if ($mins == 0) {
+		$mins = "00";
+	}
+	if ($offset < 0) {
+		$sign = "";
+	}
+	return $sign . $hours . ":" . $mins;
 }
-if(gettype($time) == "integer"){
+
+$time = "Error!";
+if (isset($_GET['time'])) {
+	if (isset($_GET['offset'])) {
+		$offset = intval($_GET['offset']) * 3600;
+	} else if (isset($_GET['offset_h']) && isset($_GET['offset_m'])) {
+		if ($_GET['offset_h'] < 0) {
+			$offset_m = intval($_GET['offset_m']);
+		} else {
+			$offset_m = -intval($_GET['offset_m']);
+		}
+
+		$offset = intval((intval($_GET['offset_h']) + $offset_m / 100) * 3600);
+	}
+	$time = strtotime($_GET['time']) - $offset;
+}
+if (gettype($time) == "integer") {
 	$prevdate = new DateTime();
 	$prevdate->setTimestamp($time + intval($_GET['offset']) * 3600);
-	$prevtime = $prevdate->format('g:ia');
-	$prevoffset = $_GET['offset'];
+	$prevtime = $prevdate->format('H:i');
+	$prevoffset = -$offset;
 } else {
 	$time = 0;
-	$prevtime = "12:00am";
-	$prevoffset = "0";
+	$prevtime = "00:00";
+	$prevoffset = 0;
 }
 ?>
 <!DOCTYPE html>
@@ -34,8 +59,12 @@ if(gettype($time) == "integer"){
 	<!--<div id="header"></div>-->
 	<div id="body">
 		<div id="timeblock">
-		Your current time is <div id="currtime" class="time"></div><br>
-		The time of the event in UTC is <div id="eventtime" class="time"></div><br>
+		Your current time is <div id="currtime" class="time"></div>
+		The time of the event in the specified localtime is <div class="time"><?=$prevtime?> GMT<?php
+echo (offsetToHrsMins($prevoffset));
+?>
+		</div>
+		The time of the event in UTC is <div id="eventtime" class="time"></div>
 		The time of the event in your local time is <div id="localeventtime" class="time"></div>
 		</div>
 		<input id="timestamp" type="hidden" value="<?=$time?>">
@@ -45,13 +74,49 @@ if(gettype($time) == "integer"){
 
 		<br>
 		<br>
-		<br>
 	</div>
 
 	<div id="footer">
 		<form id="converttimeform" action="<?=$_SERVER['PHP_SELF'];?>" method="GET">
 			Time of event: <input id="timeeventbox" type="text" name="time" value="<?=$prevtime?>"><br>
-			Time zone offset from GMT: <input id="timezonebox" value="<?=$prevoffset?>" type="text" name="offset" maxlength="6"><br>
+			Time zone offset:
+			<select name="offset_h">
+				<option value="-12">GMT-12</option>
+				<option value="-11">GMT-11</option>
+				<option value="-10">GMT-10</option>
+				<option value="-9">GMT-9</option>
+				<option value="-8">GMT-8</option>
+				<option value="-7">GMT-7</option>
+				<option value="-6">GMT-6</option>
+				<option value="-5">GMT-5</option>
+				<option value="-4">GMT-4</option>
+				<option value="-3">GMT-3</option>
+				<option value="-2">GMT-2</option>
+				<option value="-1">GMT-1</option>
+				<option value="0" selected="selected">GMT+0</option>
+				<option value="1">GMT+1</option>
+				<option value="2">GMT+2</option>
+				<option value="3">GMT+3</option>
+				<option value="4">GMT+4</option>
+				<option value="5">GMT+5</option>
+				<option value="6">GMT+6</option>
+				<option value="7">GMT+7</option>
+				<option value="8">GMT+8</option>
+				<option value="9">GMT+9</option>
+				<option value="10">GMT+10</option>
+				<option value="11">GMT+11</option>
+				<option value="12">GMT+12</option>
+				<option value="13">GMT+13</option>
+				<option value="14">GMT+14</option>
+			</select>
+			:
+			<select name="offset_m">
+				<option value="0" selected="selected">0</option>
+				<option value="25">15</option>
+				<option value="50">30</option>
+				<option value="75">45</option>
+			</select>
+			<br>
 			<input type="submit" value="Submit">
 		</form>
 
---
+++ js/converttime_nophp.js
@@ -0,0 +1,40 @@
+function formatTimeString(hours, minutes) {
+    if (hours   < 10) {hours   = "0"+hours;}
+    if (minutes < 10) {minutes = "0"+minutes;}
+    //if (seconds < 10) {seconds = "0"+seconds;}
+    return hours + ":" + minutes;
+}
+
+function get(name){
+   if(name=(new RegExp('[?&]'+encodeURIComponent(name)+'=([^&]*)')).exec(location.search))
+      return decodeURIComponent(name[1]);
+}
+
+function toUnixTimestamp(time_str, offset) {
+    var timestamp = Date.parse(time_str);
+}
+
+function showTimes(event_timestamp) {
+    // get relevant strings
+    var currdate = new Date();
+    var eventdate = new Date(parseInt(event_timestamp) * 1000);
+
+    var currtime_str = formatTimeString(currdate.getHours(), currdate.getMinutes());
+    var eventtime_utc_str = formatTimeString(eventdate.getUTCHours(), eventdate.getUTCMinutes());
+    var eventtime_str = formatTimeString(eventdate.getHours(), eventdate.getMinutes());
+
+    console.log(currtime_str);
+    console.log(eventtime_str);
+
+    $("#currtime").text(currtime_str);
+    $("#eventtime").text(eventtime_utc_str);
+    $("#localeventtime").text(eventtime_str);
+
+}
+
+$(function(){
+    // get parameters from url
+    var event_time = get("time");
+    var event_tz_offset = get("offset");
+    
+});
